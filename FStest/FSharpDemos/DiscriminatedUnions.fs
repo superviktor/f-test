3d3ec5a20ea9c7de46f8f5a20658f802fff6cabc
Viktor Prykhidko <viktor.prykhidko@softwarium.net>
2020-11-12 19:58:21 +0200

    Pattern mathing; Discriminated unions

---
+++ FStest/FSharpDemos/DiscriminatedUnions.fs
@@ -0,0 +1,61 @@
+﻿module DiscriminatedUnions
+    
+    type Suit = 
+        | Heart
+        | Diamond
+        | Spade 
+        | Club 
+
+    type PlayingCard = 
+        | Ace of Suit
+        | King of Suit
+        | Queen of Suit
+        | Jack of Suit
+        | ValueCard of int * Suit
+        member this.Value =
+            match this with 
+            | Ace(_)
+                -> 11
+            | King(_)
+            | Queen(_)
+            | Jack(_)
+                -> 10
+            | ValueCard(x,_) when x <= 10 && x >= 2
+                -> x
+            | ValueCard(_)
+                -> failwith "Card with invalida value"
+            
+
+    let deckOfCards = 
+        [
+            for suit in [Heart; Diamond; Spade; Club] do
+                yield Ace(suit)
+                yield King(suit)
+                yield Queen(suit)
+                yield Jack(suit)
+                for value in 2..10 do 
+                    yield ValueCard(value, suit)
+        ]
+
+    //describe hole cards 
+    //describeHoleCards [ValueCard(2, Heart); ValueCard(2, Heart)]
+    let describeHoleCards cards = 
+        match cards with 
+        | []
+        | [_]
+            -> failwith "too few cards"
+        | cards when cards.Length > 2 
+            -> failwith "too many cards"
+        | [Ace(_); Ace(_)]
+            -> "Pocket Rockets"
+        | [King(_); King(_)]
+            -> "Cowboy"
+        | [ValueCard(2,_); ValueCard(2,_)]
+            -> "Ducks"
+        | [Queen(_); Queen(_)]
+        | [Jack(_); Jack(_)]
+            -> "Pair of face cards"
+        | [ValueCard(x,_); ValueCard(y, _)] when x = y 
+            -> "Pair"
+        | [first; second] 
+            -> sprintf "Two cards %A and %A" first second
---
+++ FStest/FSharpDemos/PatternMatching.fs
@@ -0,0 +1,93 @@
+﻿module PatternMatching
+
+    let isOdd x = x % 2 = 1
+
+    let describeNumber x =
+        match isOdd x with 
+        | true -> printfn "%d is odd" x
+        | false -> printfn "%d is even" x
+
+    let booleanAnd x y =
+        match x y with 
+        | true, true -> true
+        | _, _ -> false
+
+    //named patterns
+    let greet x =
+        match x with 
+        | "Robert" -> printfn "Hi, Bob"        
+        | "William" -> printfn "Hi, Bill"
+        | x -> printfn "Hi, %s" x
+
+    //matching literals
+    [<Literal>]
+    let Bill = "Bill Gates"
+
+    let greatV2 x = 
+        match x with
+        | Bill -> printfn "Hi, Bill"
+        | x -> printfn "Hi, %s" x
+
+    //when guards
+    open System
+    let highLowGame () =
+        let rng = new Random()
+        let secretNumber = rng.Next() % 100
+        let rec highLowGameStep () =
+            printfn "Guess the secret number:"
+            let guessStr = Console.ReadLine()
+            let guess = Int32.Parse(guessStr)
+            match guess with
+            | _ when guess > secretNumber
+                -> printfn "The secret number is lower."
+                   highLowGameStep()
+            | _ when guess = secretNumber
+                -> printfn "You've guessed correctly!"
+                   ()
+            | _ when guess < secretNumber
+                -> printfn "The secret number is higher."
+                   highLowGameStep()
+
+        highLowGameStep()
+
+    //grouping pattern 
+    let vowelTest c =
+        match c with 
+        | 'a' | 'b' | 'c' 
+            -> true
+        | _ 
+            -> false
+
+    let describeNumbers x y =
+        match x, y with 
+        | 1, _
+        | _, 1
+            -> printfn "One of numbers is one"
+        | (2,_) & (_,2)
+            -> printfn "Both numbers are two"
+        | _ 
+            -> printfn "Other"
+
+    //tuple
+    let testXor x y =
+        match x, y with
+        | tuple when fst tuple <> snd tuple
+            -> true
+        | true, true -> false
+        | false, false -> false 
+
+    //list 
+    let rec listLengh l =
+        match l with 
+        | [] 
+            -> 0
+        | [_] 
+            -> 1
+        | [_;_] 
+            -> 2
+        | [_;_;_] 
+            -> 3
+        | hd :: tail 
+            -> 1 + listLengh tail
+
+
