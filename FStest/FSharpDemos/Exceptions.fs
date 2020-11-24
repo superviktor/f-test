9d913d9b0e674a839e841f5c79af505f34582dcd
Viktor Prykhidko <viktor.prykhidko@softwarium.net>
2020-11-24 18:30:58 +0200

    Exceptions

---
+++ FStest/FSharpDemos/Exceptions.fs
@@ -0,0 +1,23 @@
+﻿module Exceptions
+
+    //failwith
+    let divide1 x y =
+        if y = 0 then failwithf "Can't divide %d by zero" x
+        x / y
+
+    //throw
+    let divide2 x y =
+        if y = 0 then raise <| new System.DivideByZeroException()
+        x / y
+
+    //handle exception
+    let complexFunc =
+        try
+            printfn "some complex stuff"
+        with
+        | :? System.DivideByZeroException
+            -> printfn "DivideByZeroException"
+        | :? System.ArgumentNullException
+            -> printfn "DivideByZeroException"
+        | _ as ex 
+            -> printfn "%s" ex.Message
---
+++ FStest/FSharpDemos/ForLoopWithPatternMathing.fs
@@ -0,0 +1,9 @@
+﻿module ForLoopWithPatternMathing
+    type Pet =
+        | Cat of string * int //name, lives
+        | Dog of string
+
+    let famousPets = [ Dog("Lassie"); Cat("Felix", 9); Dog("Rin Tin Tin") ]
+    let printFamousDogs pets = 
+        for Dog(name) in pets do
+            printfn "The famous dog was %s" name
