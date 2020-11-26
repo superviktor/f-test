25a3dec5c247aa86802cdfc54deec3cf8c3c43ed
Viktor Prykhidko <viktor.prykhidko@softwarium.net>
2020-11-26 19:12:26 +0200

    FizzBuzz

---
+++ FStest/FSharpDemos/FizzBuzzer.fs
@@ -0,0 +1,13 @@
+﻿module FizzBuzzer
+let fizzBuzz i =
+    match i with
+    | _ when i % 3 = 0 && i % 5 = 0 
+        -> "FizzBuzz"
+    | _ when i % 3 = 0 
+        -> "Fizz"
+    | _ when i % 5 = 0
+        -> "Buzz"
+    | _ 
+        -> sprintf "%d" i
+
+    //[1..100] |> List.map fizzBuzz
